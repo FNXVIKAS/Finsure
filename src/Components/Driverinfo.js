1cce3f391e3deca6bbc2b97541dacff6d25a5489
FNXVIKAS <vikas@finexusinc.com>
2021-08-03 12:57:14 +0530

    New Commit

--- src/Components/Driverinfo.js
+++ src/Components/Driverinfo.js
@@ -1,10 +1,11 @@
 import React,{useState,useEffect} from 'react'
 import TextField from '@material-ui/core/TextField';
-import {Stepper,Step,StepLabel,Typography,Button,FormControlLabel,Checkbox} from '@material-ui/core';
+import {Button} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import { useHistory } from "react-router-dom";
+import Menu from './Menu'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -30,9 +31,17 @@ history.push("/5")
     const [dob,setDob] = useState();
     const [gender,setGender] = useState();
     const [licenseNumber,setLicenseNumber] = useState();
+    const [licenseStatus,setLicenseStatus] = useState();
+    const [licenseSuspension,setLicenseSuspension] =useState();
+    const[derogatoryReport,setDerogatoryReport] = useState();
+    const [education,setEducation] = useState();
+    const [licenseAge,setLicenseAge] = useState(0);
     const [makes,setMakes] = useState([]);
     const [models,setModels] = useState([]);
     const [years,setYears] = useState([]);
+
+    console.log("gender",gender);
+    console.log(licenseNumber,"license");
     useEffect(() => {
       var json_string = localStorage.getItem('document');
       console.log('json sads',json_string);
@@ -56,8 +65,132 @@ history.push("/5")
     console.log(firstName);
     console.log(dob);
     return (
-        <div>
-            <div className="Container">
+      <div style={{ display: "flex" }}>
+      <div style={{ width: "20%", backgroundColor: "white", margin: "15px", borderRadius: "10px" }}>
+          <Menu />
+      </div>
+      <div style={{ width: "73%", margin: "15px" }}>
+      <div style={{ padding: "15px 10px",marginRight:"20px",display: "flex",position:"relative",fontFamily: "Inter",fontStyle: "normal",fontWeight: "500",fontSize: "25px"}} onClick={Previous}><ArrowBackIcon  style={{paddingRight:"10px",paddingTop: "2px"}}/> Driver Details</div>
+      <div style={{ width: "100%", backgroundColor: "white", borderRadius: "10px", padding: "15px" }}>
+        <div style={{display:"flex",  flexWrap: "wrap"}}>
+          <div className="driveDetail">
+          <lable style={{color:"rgba(10, 33, 62, 0.6)",fontSize: "14px"}}>First Name</lable>
+                  <br></br>
+                  <input style={{height:"50px",width:"100%",background: "rgba(251, 252, 253, 1)",border: "1px solid rgba(10, 33, 62, 0.1)",borderRadius: "5px",fontSize:"20px"}} type="text" value={firstName} onChange={e=>setFirstName(e.target.value)}></input>
+               
+          </div>
+          <div className="driveDetail">
+          <lable style={{color:"rgba(10, 33, 62, 0.6)",fontSize: "14px"}}>Last Name</lable>
+                  <br></br>
+                  <input style={{height:"50px",width:"100%",background: "rgba(251, 252, 253, 1)",border: "1px solid rgba(10, 33, 62, 0.1)",borderRadius: "5px",fontSize:"20px"}} type="text" value={lastName} onChange={e=>setLastName(e.target.value)}></input>
+                
+          </div>
+          <div className="driveDetail">
+          <lable style={{color:"rgba(10, 33, 62, 0.6)",fontSize: "14px"}}>Date Of Birth</lable>
+                  <br></br>
+                  <input style={{height:"50px",width:"100%",background: "rgba(251, 252, 253, 1)",border: "1px solid rgba(10, 33, 62, 0.1)",borderRadius: "5px",fontSize:"20px"}}type="date" value={dob} onChange={e=>setDob(e.target.value)}></input>
+             
+          </div>
+     
+        </div>
+        <div style={{display:"flex",  flexWrap: "wrap"}}>
+          <div className="driveDetail">
+          <lable style={{color:"rgba(10, 33, 62, 0.6)",fontSize: "14px"}}>Gender</lable>
+                  <br></br>
+                <select style={{height:"50px",width:"100%",background: "rgba(251, 252, 253, 1)",border: "1px solid rgba(10, 33, 62, 0.1)",borderRadius: "5px",fontSize:"20px"}} value={gender} onChange={e=>setGender(e.target.value)}>
+                      <option value="male">Male</option>
+                      <option value="female">Female</option>
+                      <option value="other">Non-Binary</option>
+                </select>
+          </div>
+          <div className="driveDetail">
+          <lable style={{color:"rgba(10, 33, 62, 0.6)",fontSize: "14px"}}>License Number</lable>
+                  <br></br>
+                  <input style={{height:"50px",width:"100%",background: "rgba(251, 252, 253, 1)",border: "1px solid rgba(10, 33, 62, 0.1)",borderRadius: "5px",fontSize:"20px"}} type="number" value={licenseNumber} onChange={e=>setLicenseNumber(e.target.value)}></input>
+              
+          </div>
+          <div className="driveDetail">
+          <lable style={{color:"rgba(10, 33, 62, 0.6)",fontSize: "14px"}}>License Status</lable>
+                  <br></br>
+                  <select style={{height:"50px",width:"100%",background: "rgba(251, 252, 253, 1)",border: "1px solid rgba(10, 33, 62, 0.1)",borderRadius: "5px",fontSize:"20px"}} name="cars" id="cars">
+                      <option value="active">Active</option>
+                      <option value="permit">Permit</option>
+                      <option value="suspended">Suspended</option>
+                      <option value="foreign">Foreign</option>
+                      <option value="expired">Expired</option>
+                </select>
+          </div>
+     
+        </div>
+
+        <div style={{display:"flex",  flexWrap: "wrap"}}>
+          <div className="driveDetail">
+          <lable style={{color:"rgba(10, 33, 62, 0.6)",fontSize: "14px"}}>Any Licens Suspensions?</lable>
+                  <br></br>
+                  <select style={{height:"50px",width:"100%",background: "rgba(251, 252, 253, 1)",border: "1px solid rgba(10, 33, 62, 0.1)",borderRadius: "5px",fontSize:"20px"}} name="cars" id="cars">
+                      <option value="no">No</option>
+                      <option value="yes">Yes</option>
+                </select>
+          </div>
+          <div className="driveDetail">
+          <lable style={{color:"rgba(10, 33, 62, 0.6)",fontSize: "14px"}}>Any Derogatory Report?</lable>
+                  <br></br>
+                  <select style={{height:"50px",width:"100%",background: "rgba(251, 252, 253, 1)",border: "1px solid rgba(10, 33, 62, 0.1)",borderRadius: "5px",fontSize:"20px"}} name="cars" id="cars">
+                      <option value="no">No</option>
+                      <option value="yes">Yes</option>
+                </select>
+          </div>
+          <div className="driveDetail">
+          <lable style={{color:"rgba(10, 33, 62, 0.6)",fontSize: "14px"}}>Age When obtained Driver’s license</lable>
+                  <br></br>
+                  <input style={{height:"50px",width:"100%",background: "rgba(251, 252, 253, 1)",border: "1px solid rgba(10, 33, 62, 0.1)",borderRadius: "5px",fontSize:"20px"}}type="Number"></input>
+               
+          </div>
+     
+        </div>
+
+        <div style={{display:"flex",  flexWrap: "wrap"}}>
+          <div className="driveDetail">
+          <lable style={{color:"rgba(10, 33, 62, 0.6)",fontSize: "14px"}}>Highest Level of Education</lable>
+                  <br></br>
+                  <select style={{height:"50px",width:"100%",background: "rgba(251, 252, 253, 1)",border: "1px solid rgba(10, 33, 62, 0.1)",borderRadius: "5px",fontSize:"20px"}} name="cars" id="cars">
+                  <option selected="selected">Select Education</option>
+                      <option value="graduate">Graduate</option>
+                      <option value="undergraduate">Under-Graduate</option>
+                      <option value="postgraduate">Post-Graduate</option>
+
+                </select>
+          </div>
+          <div className="driveDetail">
+          <lable style={{color:"rgba(10, 33, 62, 0.6)",fontSize: "14px"}}>Car Make</lable>
+                  <br></br>
+                  <select style={{height:"50px",width:"100%",background: "rgba(251, 252, 253, 1)",border: "1px solid rgba(10, 33, 62, 0.1)",borderRadius: "5px",fontSize:"20px"}} name="cars" id="cars">
+                  {makes.map((make)=>{
+                    return <option value={make}>{make}</option>
+                  })}
+                  
+                </select>
+          </div>
+          <div className="driveDetail">
+          <lable style={{color:"rgba(10, 33, 62, 0.6)",fontSize: "14px"}}>Car Model</lable>
+                  <br></br>
+                  <select style={{height:"50px",width:"100%",background: "rgba(251, 252, 253, 1)",border: "1px solid rgba(10, 33, 62, 0.1)",borderRadius: "5px",fontSize:"20px"}} name="cars" id="cars">
+                  
+                  {models.map((model)=>{
+                    return <option value={model}>{model}</option>
+                  })}
+                </select>    
+          </div>
+     
+        </div>
+
+      </div>
+      <div style={{position:"absolute",right:"0px",margin:"20px"}}>
+                <Button style={{ backgroundColor: "#ECF0F3", padding: "15px 10px" ,color:"rgba(10, 33, 62, 0.4)",marginRight:"20px"}} onClick={Previous}><ArrowBackIcon  style={{paddingRight:"10px"}}/> Previous</Button>
+      <Button variant="contained" style={{ backgroundColor: "#1AC29A", padding: "15px 10px", color: "white"}} onClick={Continue}>Proceed to Next step <ArrowForwardIcon style={{paddingLeft:"10px"}} /></Button>
+    </div>
+      </div>
+          {/*  <div className="Container">
                 <div style={{marginBottom:"25px"}}> 
                     <div className="check">
                     <lable style={{color:"rgba(10, 33, 62, 0.6)",fontSize: "14px"}}>First Name</lable>
@@ -159,22 +292,8 @@ history.push("/5")
                 </div>
                 </div>
             </div>
-            <div style={{ marginLeft: "65%" }}>
-        <Button style={{ backgroundColor: "#ECF0F3", padding: "15px 10px" ,color:"rgba(10, 33, 62, 0.4)"}} onClick={Previous} className={classes.button}>
-        <ArrowBackIcon  style={{paddingRight:"10px"}}/> Previous
-        </Button>
-        <Button
-          style={{ backgroundColor: "#1AC29A", padding: "15px 10px", color: "white" }}
-          variant="contained"
-          color="primary"
-          onClick={Continue}
-          className={classes.button}
-        >
-
-          Proceed to Next step
-  <ArrowForwardIcon style={{paddingLeft:"10px"}} />
-        </Button>
-          </div>
+                */}
+          
         </div>
     )
 }
--- src/Components/Insurancedetails.js
+++ src/Components/Insurancedetails.js
@@ -5,6 +5,7 @@ import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from "react-router-dom";
+import Menu from './Menu'
 
 const useStyles = makeStyles((theme) => ({
     formControl: {
@@ -31,6 +32,77 @@ const Insurancedetails = (props) => {
     const [currentCarrier, setCurrentCarrier] = useState();
     console.log(long)
     return (
+        
+      <div style={{ display: "flex" }}>
+      <div style={{ width: "20%", backgroundColor: "white", margin: "15px", borderRadius: "10px" }}>
+          <Menu />
+      </div>
+      <div style={{ width: "73%", margin: "15px"}}>
+      <div style={{ padding: "15px 10px",marginRight:"20px",display: "flex",position:"relative",fontFamily: "Inter",fontStyle: "normal",fontWeight: "500",fontSize: "25px"}} onClick={Previous}><ArrowBackIcon  style={{paddingRight:"10px",paddingTop: "2px"}}/>Insurance Details</div>
+     <div style={{ width: "100%", backgroundColor: "white", borderRadius: "10px", padding: "25px" }}>
+     <p><b>Who is your current Insurance carrier?</b></p>
+        <div style={{display:"flex",padding:"20px",flexWrap:"wrap"}}>
+        
+         <div className= {currentCarrier === 'AllState' ? 'priorInsuranceclick' : 'priorInsurance'} onClick={() => setCurrentCarrier('AllState')}>
+                        <div className= {currentCarrier === 'AllState' ? 'tikclick' : 'tik'}>
+                        <CheckIcon />
+                        </div>
+         </div>
+         <div className= {currentCarrier === 'BristolWest' ? 'priorInsuranceclick' : 'priorInsurance'} onClick={() => setCurrentCarrier('BristolWest')}>
+                        <div className= {currentCarrier === 'BristolWest' ? 'tikclick' : 'tik'}>
+                        <CheckIcon />
+                        </div>
+                    </div>
+                    <div className= {currentCarrier === 'Farmers' ? 'priorInsuranceclick' : 'priorInsurance'} onClick={() => setCurrentCarrier('Farmers')}>
+                        <div className= {currentCarrier === 'Farmers' ? 'tikclick' : 'tik'}>
+                        <CheckIcon />
+                        </div>
+                    </div>
+                    <div className= {currentCarrier === 'Infinity' ? 'priorInsuranceclick' : 'priorInsurance'} onClick={() => setCurrentCarrier('Infinity')}>
+                        <div className= {currentCarrier === 'Infinity' ? 'tikclick' : 'tik'}>
+                        <CheckIcon />
+                        </div>
+                    </div>
+                    <div className= {currentCarrier === 'Mercury' ? 'priorInsuranceclick' : 'priorInsurance'} onClick={() => setCurrentCarrier('Mercury')}>
+                        <div className= {currentCarrier === 'Mercury' ? 'tikclick' : 'tik'}>
+                        <CheckIcon />
+                        </div>
+                    </div>
+                    <div className= {currentCarrier === 'Root' ? 'priorInsuranceclick' : 'priorInsurance'} onClick={() => setCurrentCarrier('Root')}>
+                        <div className= {currentCarrier === 'Root' ? 'tikclick' : 'tik'}>
+                        <CheckIcon />
+                        </div>
+                    </div>
+
+        </div>
+        <hr></hr>
+        <p><b>How long have you been insured?</b></p>
+        <div style={{display:"flex",padding:"20px",flexWrap:"wrap"}}>
+        <div className={long === '5-6 years' ? 'howlonginsuredclick' : 'howlonginsured'} onClick={() => setLong('5-6 years')}>
+                        <div style={{}}>5 - 6 Years</div>
+                        <div style={{ }}><CheckIcon /></div>
+                    </div>
+                    <div className={long === '3-4 years' ? 'howlonginsuredclick' : 'howlonginsured'} onClick={() => setLong('3-4 years')}>
+                        <div style={{ display: "inline-block", marginTop: "20px" }}>3 - 4 Years</div>
+                        <div style={{ display: "inline-block", marginLeft: "20%", color: "rgba(243, 245, 248, 1)", paddingTop: "15px" }}><CheckIcon /></div>
+                    </div>
+                    <div className={long === '1-2 years' ? 'howlonginsuredclick' : 'howlonginsured'} onClick={() => setLong('1-2 years')}>
+                        <div style={{ display: "inline-block", marginTop: "20px" }}>1 - 2 Year</div>
+                        <div style={{ display: "inline-block", marginLeft: "20%", color: "rgba(243, 245, 248, 1)", paddingTop: "15px" }}><CheckIcon /></div>
+                    </div>
+                    <div className={long === '6-12 months' ? 'howlonginsuredclick' : 'howlonginsured'} onClick={() => setLong('6-12 months')}>
+                        <div style={{ display: "inline-block", marginTop: "20px" }}>6 - 12 Months</div>
+                        <div style={{ display: "inline-block", marginLeft: "15%", color: "rgba(243, 245, 248, 1)", paddingTop: "15px" }}><CheckIcon /></div>
+                    </div>
+                    <div className={long === '6 months' ? 'howlonginsuredclick' : 'howlonginsured'} onClick={() => setLong('6 months')}>
+                        <div style={{ display: "inline-block", marginTop: "20px" }}>6 Months</div>
+                        <div style={{ display: "inline-block", marginLeft: "20%", color: "rgba(243, 245, 248, 1)", paddingTop: "15px" }}><CheckIcon /></div>
+                    </div>
+            </div>
+     </div>
+      </div>
+      </div>
+        /*
         <div>
             <div className="Container">
                 <div>
@@ -106,6 +178,7 @@ const Insurancedetails = (props) => {
         </Button>
       </div>
         </div>
+        */
     )
 }
 export default Insurancedetails
